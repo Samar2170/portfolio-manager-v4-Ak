b224eb1d47ec1f1d3e4169177ee4e638ff242d63
samar <scipioresearch2020@gmail.com>
2023-10-05 01:54:25 +0530

    added cron

---
+++ jobs/cronjobs.go
@@ -0,0 +1,35 @@
+package jobs
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"github.com/samar2170/portfolio-manager-v4/security/stock"
+)
+
+var cronLogger *log.Logger
+
+func init() {
+	t := time.Now()
+
+	file, err := os.OpenFile(fmt.Sprintf("logs/Cron_logs_%d-%d-%d", t.Day(), int(t.Month()), t.Year()), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+	cronLogger = log.New(file, "[Cron]-", log.Ldate|log.Ltime|log.Lshortfile)
+	cronLogger.Println("Cron Server Working buddy")
+}
+
+func StartCronServer() {
+	t := time.Now()
+
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Every(1).Day().At("00:00").Do(func() {
+		cronLogger.Println("Updating Stock Prices" + t.String())
+		stock.UpdateStockPrices()
+	})
+}
