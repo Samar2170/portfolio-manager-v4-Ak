6309209969ecf05d9eef75e68d3f80ac80d270c9
samar <scipioresearch2020@gmail.com>
2023-10-18 20:49:49 +0530

    db persistance fix

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/sqlite"
@@ -16,26 +17,32 @@ func connect() {
 	var err error
 	if environment == "prod" {
 		DB, err = gorm.Open(sqlite.Open("db/prod.db"), &gorm.Config{})
+		// DB, err = gorm.Open(postgres.Open(DBURI), &gorm.Config{
+		// 	DisableForeignKeyConstraintWhenMigrating: true,
+		// })
 	} else {
-		DB, err = gorm.Open(sqlite.Open("db/gorm.db"), &gorm.Config{})
+		DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	}
 	if err != nil {
 		panic(err)
 	}
 }
+
 func loadEnvVars() {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 
 	DBURI = fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s sslmode=disable",
-		viper.Get("DBHOST"),
-		viper.Get("DBUSER"),
-		viper.Get("DBNAME"),
-		viper.Get("DBPASSWORD"),
-		viper.Get("DBPORT"),
+		viper.GetString("POSTGRES_HOST"),
+		viper.GetString("POSTGRES_USER"),
+		viper.GetString("POSTGRES_DB"),
+		viper.GetString("POSTGRES_PASSWORD"),
+		viper.GetString("POSTGRES_PORT"),
 	)
+	log.Println(DBURI)
 	environment = viper.GetString("ENVIRONMENT")
 }
+
 func init() {
 	loadEnvVars()
 	connect()
