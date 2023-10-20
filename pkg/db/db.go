1038351fd82889521df5992765ea89195fa8a698
samar <scipioresearch2020@gmail.com>
2023-10-20 21:22:26 +0530

    psql working

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,10 +17,10 @@ var DB *gorm.DB
 func connect() {
 	var err error
 	if environment == "prod" {
-		DB, err = gorm.Open(sqlite.Open("db/prod.db"), &gorm.Config{})
-		// DB, err = gorm.Open(postgres.Open(DBURI), &gorm.Config{
-		// 	DisableForeignKeyConstraintWhenMigrating: true,
-		// })
+		// DB, err = gorm.Open(sqlite.Open("database/prod.db"), &gorm.Config{})
+		DB, err = gorm.Open(postgres.Open(DBURI), &gorm.Config{
+			DisableForeignKeyConstraintWhenMigrating: true,
+		})
 	} else {
 		DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	}
@@ -31,16 +32,12 @@ func connect() {
 func loadEnvVars() {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
-
-	DBURI = fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s sslmode=disable",
-		viper.GetString("POSTGRES_HOST"),
-		viper.GetString("POSTGRES_USER"),
-		viper.GetString("POSTGRES_DB"),
-		viper.GetString("POSTGRES_PASSWORD"),
-		viper.GetString("POSTGRES_PORT"),
-	)
-	log.Println(DBURI)
 	environment = viper.GetString("ENVIRONMENT")
+	pgHost, pgPort := viper.GetString("POSTGRES_HOST"), viper.GetInt("POSTGRES_PORT")
+	pgUser, pgPassWord := viper.GetString("POSTGRES_USER"), viper.GetString("POSTGRES_PASSWORD")
+	pgDB := viper.GetString("POSTGRES_DB")
+	DBURI = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Kolkata", pgHost, pgUser, pgPassWord, pgDB, pgPort)
+	log.Println(DBURI)
 }
 
 func init() {
